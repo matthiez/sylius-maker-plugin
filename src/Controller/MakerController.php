66acb85a9098f491440606259ef5d1d58e43f28f
André Matthies <matthiez@gmail.com>
2019-01-07 17:00:21 +0100

    show products for maker

--- src/Controller/MakerController.php
+++ src/Controller/MakerController.php
@@ -4,8 +4,59 @@ declare(strict_types=1);
 
 namespace Ecolos\SyliusMakerPlugin\Controller;
 
+use Ecolos\SyliusMakerPlugin\Entity\MakerInterface;
+use Ecolos\SyliusMakerPlugin\Repository\MakerRepositoryInterface;
+use Sylius\Bundle\CoreBundle\Doctrine\ORM\ProductRepository;
 use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Core\Repository\ProductRepositoryInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 
-class MakerController extends ResourceController
+final class MakerController
 {
+    /**
+     * @var EngineInterface
+     */
+    private $templatingEngine;
+
+    /**
+     * @param EngineInterface $templatingEngine
+     */
+    public function __construct(EngineInterface $templatingEngine, EntityRepository $makerRepository, ProductRepositoryInterface $productRepository) {
+        $this->templatingEngine = $templatingEngine;
+        $this->makerRepository = $makerRepository;
+        $this->productRepository = $productRepository;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function showAction(Request $request): Response {
+        $slug = $request->attributes->get("slug");
+
+        /**
+         * @var MakerInterface $maker
+         */
+        $maker = $this->makerRepository->findOneBy(["slug" => $slug]);
+
+        $products = $this->productRepository->findBy(["maker" => $maker->getId()]);
+        
+/*        $products["_taxons"] = array_map(function(ProductInterface $product) {
+            return $product->getMainTaxon();
+        }, $products);*/
+        
+        return $this->templatingEngine->renderResponse(
+            '@EcolosSyliusMakerPlugin/Maker/show.html.twig',
+            [
+                "maker" => $maker,
+                "products" => $products
+            ]
+        );
+    }
 }
